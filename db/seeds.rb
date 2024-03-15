bad67b5b1af5f8d47962e9a459c3e1c95ab4acd8
aisaka-1653 <reobata1127@gmail.com>
2024-03-16 00:32:03 +0900

    Execute seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,14 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |n|
+  Product.create!(
+    name: "Item#{n + 1}",
+    content: "この商品はItem#{n + 1}です",
+    price: rand(1000..2000),
+    image: ActiveStorage::Blob.create_and_upload!(
+      io: File.open(Rails.root.join("app/assets/images/products/item#{n + 1}.jpg")),
+      filename: 'item#{n + 1}.jpg')
+  )
+end
