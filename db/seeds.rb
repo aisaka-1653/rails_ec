35e5009dfea3e98c27aff006d5740a13ee8e15a9
aisaka-1653 <reobata1127@gmail.com>
2024-03-23 12:27:50 +0900

    Apply RuboCop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -11,6 +12,7 @@
     price: rand(1000..2000),
     image: ActiveStorage::Blob.create_and_upload!(
       io: File.open(Rails.root.join("app/assets/images/products/item#{n + 1}.jpg")),
-      filename: "item#{n + 1}.jpg")
+      filename: "item#{n + 1}.jpg"
+    )
   )
 end
