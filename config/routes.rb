431f29e049f2e6c9cd94746c34743900bd042a65
aisaka-1653 <reobata1127@gmail.com>
2024-03-24 21:19:57 +0900

    rubocopの適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products, only: [:index, :show]
+  resources :products, only: %i[index show]
   namespace :admin do
-    resources :products, only: [:index, :new, :edit, :create, :update, :destroy]
+    resources :products, only: %i[index new edit create update destroy]
   end
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
