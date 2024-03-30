2f368ac64e45d785b87d076cca91587285e1d85c
aisaka-1653 <reobata1127@gmail.com>
2024-03-30 21:45:31 +0900

    javascriptのイベント名を更新

--- app/javascript/controllers/cart_form_validation.js
+++ app/javascript/controllers/cart_form_validation.js
@@ -2,7 +2,7 @@
 (function () {
   'use strict'
 
-  window.addEventListener('load', function () {
+  window.addEventListener('turbo:load', function () {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation')
 
