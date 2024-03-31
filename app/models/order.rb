c473db398a0dddfe00effcd416f2f0b05bce36b9
aisaka-1653 <reobata1127@gmail.com>
2024-03-31 23:08:23 +0900

    Orderモデルにvalidationを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,40 @@
 class Order < ApplicationRecord
+  has_many :order_detail, dependent: :destroy
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :username, format: {
+      with: /\A\w+\z/i,
+      message: "英数字のみ有効です"
+    }
+    validates :email, format: {
+      with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+      message: "メールアドレスの値が不正です"
+    }
+    validates :zip_code, format: {
+      with: /\A\d{7}\z/,
+      message: "郵便番号は7桁の数字のみ有効です"
+    }
+    validates :country
+    validates :state
+    validates :primary_address
+    validates :secondary_address
+    validates :credit_name, format: {
+      with: /\A[a-z]+\s?[a-z]\z/i,
+      message: "名義人は英文字のみ有効です"
+    }
+    validates :credit_number, format: {
+      with: /\A\d{14,16}\z/,
+      message: "クレジットカード番号は14~16桁の数字のみ有効です"
+    }
+    validates :credit_expiration, format: {
+      with: /\A\d{2}\/\d{2}\z/,
+      message: "MM/YYの形式のみ有効です"
+    }
+    validates :credit_cvv,format: {
+      with: /\A\d{3,4}\z/,
+      message: "3~4桁の数字のみ有効です"
+    }
+  end
 end
