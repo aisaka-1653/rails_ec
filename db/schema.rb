1df0e8faa5206c77e0fc26e2f5597bd24d39cb3c
aisaka-1653 <reobata1127@gmail.com>
2024-03-31 12:08:20 +0900

    OrderDatailモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_31_025721) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_31_030559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_31_025721) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -95,4 +105,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_31_025721) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
+  add_foreign_key "order_details", "orders"
+  add_foreign_key "order_details", "products"
 end
