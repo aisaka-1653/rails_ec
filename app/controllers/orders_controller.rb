09d99b0993156f2661d71751e06afdac3691a0cc
aisaka-1653 <reobata1127@gmail.com>
2024-04-05 17:08:38 +0900

    カートページにvalidationメッセージを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,18 @@
 class OrdersController < ApplicationController
   def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      redirect_to root_path, notice: "購入ありがとうございます"
+    else
+      @cart_items = current_cart.cart_items
+      render "carts/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit!
   end
 end
