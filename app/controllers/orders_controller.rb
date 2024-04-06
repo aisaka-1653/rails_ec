ff6dabf45c0ce65a1663c5deeecd2e858f7fddde
aisaka-1653 <reobata1127@gmail.com>
2024-04-06 17:21:45 +0900

    カート削除機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,20 +4,19 @@ class OrdersController < ApplicationController
     @cart_items = current_cart.cart_items.includes(:product)
 
     @order.transaction do
-      if @order.save
-        @cart_items.each do |cart_item|
-          product = cart_item.product
-          @order.order_details.create!(
-            product_name: product.name,
-            product_price: product.price,
-            quantity: cart_item.quantity,
-            subtotal: cart_item.subtotal
-          )
-        end
-        redirect_to root_path, notice: "購入ありがとうございます"
-      else
-        render "carts/show", status: :unprocessable_entity
+      @order.save!
+      @cart_items.each do |cart_item|
+        product = cart_item.product
+        @order.order_details.create!(
+          product_name: product.name,
+          product_price: product.price,
+          quantity: cart_item.quantity,
+          subtotal: cart_item.subtotal
+        )
       end
+      current_cart.destroy
+      session.delete(:cart_id)
+      redirect_to root_path, notice: "購入ありがとうございます"
     end
   rescue ActiveRecord::RecordInvalid
     flash.now[:error] = "注文が正常に行えませんでした"
