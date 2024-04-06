c9e182052ff1febb49217087f1259944536cce60
aisaka-1653 <reobata1127@gmail.com>
2024-04-06 10:58:06 +0900

    validationのリファクタリング

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,40 +1,20 @@
 class Order < ApplicationRecord
   has_many :order_detail, dependent: :destroy
 
-  with_options presence: true do
-    validates :first_name
-    validates :last_name
-    validates :user_name, format: {
-      with: /\A\w+\z/i,
-      message: "は英数字のみ有効です"
-    }
-    validates :email, format: {
-      with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
-      message: "の値が不正です"
-    }
-    validates :zip_code, format: {
-      with: /\A\d{7}\z/,
-      message: "は7桁の数字のみ有効です"
-    }
-    validates :country
-    validates :state
-    validates :primary_address
-    validates :secondary_address
-    validates :credit_name, format: {
-      with: /\A[a-z]+\s?[a-z]+\z/i,
-      message: "は英文字のみ有効です"
-    }
-    validates :credit_number, format: {
-      with: /\A\d{4}(?: \d{4}){3}\z/,
-      message: "は16桁の数字のみ有効です"
-    }
-    validates :credit_expiration, format: {
-      with: /\A\d{2}\/\d{2}\z/,
-      message: "はMM/YYの形式のみ有効です"
-    }
-    validates :credit_cvv,format: {
-      with: /\A\d{3,4}\z/,
-      message: "は3~4桁の数字のみ有効です"
-    }
+  validates :first_name, :last_name, :user_name, :email, :zip_code, :country, :state,
+            :primary_address, :secondary_address, :credit_name, :credit_number,
+            :credit_expiration, :credit_cvv, presence: true
+
+  validates :user_name, format: { with: /\A\w+\z/i }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :zip_code, format: { with: /\A\d{7}\z/ }
+  validates :credit_name, format: { with: /\A[a-z]+\s?[a-z]+\z/i }
+  validates :credit_number, format: { with: /\A\d{4}(?: \d{4}){3}\z/ }
+  validates :credit_cvv, format: { with: /\A\d{3,4}\z/ }
+
+  def credit_expiration=(value)
+    if value.match?(/\A\d{2}\/\d{2}\z/)
+      self[:credit_expiration] = Date.strptime(value, "%m/%y").end_of_month
+    end
   end
 end
