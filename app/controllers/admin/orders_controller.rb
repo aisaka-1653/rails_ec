4a1947daf9f57840a3720964af5b5a12810ff302
aisaka-1653 <reobata1127@gmail.com>
2024-04-07 13:25:59 +0900

    購入明細一覧ページの実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   include BasicAuthenticatable
 
   def index
+    @orders = Order.includes(:order_details).order(created_at: :desc)
   end
 
   def show
--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,2 +1,5 @@
 module Admin::OrdersHelper
+  def purchased_items_count(order)
+    order.order_details.count
+  end
 end
