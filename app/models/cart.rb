56a02b1e170add41310112dd4714c6660f415561
aisaka-1653 <reobata1127@gmail.com>
2024-04-12 14:42:09 +0900

    カート削除時にプロモコードも削除する

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
-  belongs_to :promotion_code, optional: true
+  belongs_to :promotion_code, optional: true, dependent: :destroy
 
   def total_price
     cart_items.includes(:product).sum(&:subtotal) - (promotion_code&.amount || 0)
