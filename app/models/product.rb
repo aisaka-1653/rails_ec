f9175c46604006a4f42136e99605ba1d73cd3f22
aisaka-1653 <reobata1127@gmail.com>
2024-04-12 23:19:15 +0900

    Productモデルのアソシエーション修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@
 
 class Product < ApplicationRecord
   has_one_attached :image
-  has_many :item_carts, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
   validates :name, :content, :price, presence: true
   validates :price, numericality: { only_integer: true }
